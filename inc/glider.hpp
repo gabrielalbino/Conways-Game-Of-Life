2c73aeee51d2600b409da66543608fa2da5d0ab8
Gabriel Albino <gabrielalbino@aluno.unb.br>
2017-09-07 20:39:28 -0300

    Adcionado método deleteAGlider

--- inc/glider.hpp
+++ inc/glider.hpp
@@ -7,5 +7,6 @@
 class Glider : public Universo{
 public:
 	CelulaMatrix makeAGlider(CelulaMatrix universoAntigo, int x, int y);	
+	CelulaMatrix deleteAGlider(CelulaMatrix universoAntigo, int x, int y);
 };
 #endif
