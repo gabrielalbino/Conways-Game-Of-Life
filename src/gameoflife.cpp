bf8f98788baec500f50e5a43244f311e49f4c924
Gabriel Albino <gabrielalbino@aluno.unb.br>
2017-09-07 19:16:19 -0300

    adcionado a opção de adcionar um glider e uma celula ao universo celular

--- src/gameoflife.cpp
+++ src/gameoflife.cpp
@@ -1,17 +1,27 @@
 #include "gameoflife.hpp"
 #include "block.hpp"
 #include "blinker.hpp"
+#include "glider.hpp"
 
 void GameOfLife::setForma(int forma, int x, int y){
 	switch(forma){
 	case 1:{
+		if(x >= 0 && y >= 0 && x < ALTURA && y < LARGURA)
+			getUniverso()[x][y].setAlive(true);
+	}
+	case 2:{
 		Block blockObj;
 		setUniverso(blockObj.makeABlock(getUniverso(), x, y));
 		break;
 	}
-	case 2:{
+	case 3:{
 		Blinker blinkerObj;
 		setUniverso(blinkerObj.makeABlinker(getUniverso(),x,y));
+		break;
+	}
+	case 4:{
+		Glider gliderObj;
+		setUniverso(gliderObj.makeAGlider(getUniverso(),x,y));
 		break;
 	}
 	}
